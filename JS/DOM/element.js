a55712ad194beb547f5e355fc86820f34cb83bea
crybaby050 <benthiam750@gmail.com>
2026-04-28 15:37:04 +0000

    Recuperation des elements html

---
+++ JS/DOM/element.js
@@ -0,0 +1,34 @@
+//Sidebar
+export const sidebar               = document.getElementById("sidebar");
+
+//Header
+
+//bouton pour le sidebar
+export const btnSidebarDesktop     = document.getElementById("btn-toggle-sidebar-desktop");
+export const btnSidebarMobile      = document.getElementById("btn-toggle-sidebar-mobile");
+
+// Banner
+export const btnAjout              = document.getElementById("btn-ajout");
+
+// Filtres
+export const searchInput           = document.getElementById("search-input");
+export const filterFormation       = document.getElementById("filter-formation");
+export const btnReinitialiser      = document.getElementById("btn-reinitialiser");
+export const btnRestaurer          = document.getElementById("btn-restaurer");
+
+// Bascule de vue 
+export const btnVueTableau         = document.getElementById("btn-vue-tableau");
+export const btnVueCartes          = document.getElementById("btn-vue-cartes");
+export const viewTableau           = document.getElementById("view-tableau");
+export const viewCartes            = document.getElementById("view-cartes");
+
+//Tableau & card
+export const tableBody             = document.getElementById("table-body");
+export const cardGrid              = document.getElementById("card-grid");
+
+//Overlay and toast
+export const overlay               = document.getElementById("overlay");
+export const toastContainer        = document.getElementById("toast-container");
+
+//Pagination
+export const pagina = document.getElementById("pagination")
