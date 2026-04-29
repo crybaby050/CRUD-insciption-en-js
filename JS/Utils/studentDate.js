f646c8951a2a37abdc3bbe390d211faf27c81d68
crybaby050 <benthiam750@gmail.com>
2026-04-29 13:36:32 +0000

    Ajout de format de date

---
+++ JS/Utils/studentDate.js
@@ -0,0 +1,8 @@
+export function formatTaskDate(date = new Date()) {
+    return date.toLocaleString("fr-FR", {
+        day: "2-digit",
+        month: "short",
+        hour: "2-digit",
+        minute: "2-digit",
+    });
+}
